cfadd7d9b486590d0008ea6627bc3f12363d0541
Rodolpho Alves <rodolpho@rdca.dev.br>
2025-12-24 17:22:52 -0300

    log when calling pings.controller.ts

--- apps/pinger/src/pings/pings.controller.ts
+++ apps/pinger/src/pings/pings.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { PingsService } from './pings.service';
 
-
 @Controller('pings')
 export class PingsController {
   private readonly logger = new Logger(PingsController.name);
 
-  constructor(private readonly pingService: PingsService) {
-
-  }
+  constructor(private readonly pingService: PingsService) {}
 
   @Get()
   allPings() {
+    this.logger.debug('fetching all pings');
     return [];
   }
 }
