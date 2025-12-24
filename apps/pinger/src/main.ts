4eda3bd676eee34711b2f2043e4e31e4961c3ea2
Rodolpho Alves <rodolpho@rdca.dev.br>
2025-12-24 17:09:00 -0300

    rename pinger module to appModule

--- apps/pinger/src/main.ts
+++ apps/pinger/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { PingerModule } from './pinger.module';
+import { AppModule } from './appModule';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PingerModule);
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
--- apps/pinger/src/pinger.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PingerService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
