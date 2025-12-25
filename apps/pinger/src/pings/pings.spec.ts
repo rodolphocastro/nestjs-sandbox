7f18d0f67c564a241804aace7673b397bfe7321d
Rodolpho Alves <rodolpho@rdca.dev.br>
2025-12-25 10:23:34 -0300

    refactor all specs into a single spec

--- apps/pinger/src/pings/pings.spec.ts
+++ apps/pinger/src/pings/pings.spec.ts
@@ -3,7 +3,33 @@ import {
   ValkeyContainer,
 } from '@testcontainers/valkey';
 import Valkey from 'iovalkey';
+
 import { ValkeyPingsRepository } from './pings.repository';
+import { Ping } from './pings.entity';
+
+describe('PingsEntity', () => {
+  it('should have a Title and a Creation Date', () => {
+    const { Title, CreatedOn } = new Ping('hello, world', new Date(), false);
+    expect(Title).toBe('hello, world');
+    expect(CreatedOn).toBeDefined();
+  });
+
+  describe('acknowledgement', () => {
+    it('should allow ack to change its acknowledgement status', () => {
+      const got = new Ping('hello, world', new Date(), false);
+      expect(got.IsAcknowledged).toBe(false);
+      got.ack();
+      expect(got.IsAcknowledged).toBe(true);
+    });
+
+    it('should not allow ack to change its acknowledgement status if already acknowledged', () => {
+      const got = new Ping('hello, world', new Date(), true);
+      expect(got.IsAcknowledged).toBe(true);
+      got.ack();
+      expect(got.IsAcknowledged).toBe(true);
+    });
+  });
+});
 
 describe('PingsRepository', () => {
   let valkeyContainer: StartedValkeyContainer;
